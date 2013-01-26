23d21ebba6dbcc4d5f89330f14b56464c7e6dc46
Alden Quimby <aldenquimby@gmail.com>
2013-01-26 14:11:08 -0500

    maybe better?

--- assignment1/pattern_matcher.lisp
+++ assignment1/pattern_matcher.lisp
@@ -1,6 +1,46 @@
 ;;; pattern_matcher.lisp
 ;;; a simple pattern matcher
 
+;; driver to test algorithm
+(defun test-match ()
+	(test-one-match '() '() t)
+	(test-one-match '(ai) '(ai) t)
+	(test-one-match '(ai cs) '(ai cs) t)
+	(test-one-match '(cs ai) '(ai cs) NIL)
+	(test-one-match '(1 2 3 0) '(1 2 3 4 0) NIL)
+	(test-one-match '(? mudd) '(seely mudd) t)
+	(test-one-match '(?first ?middle mudd) '(seely w mudd) '((?middle w) (?first seely)))
+	(test-one-match '(? ?x ? ?y ?) '(Warren Buffet Is A Good Man) NIL)
+	(test-one-match '(School Of Engineering and Applied Science) '(School Of Engineering) NIL)
+	(test-one-match '(* School Of Engineering and Applied Science) '(The Fu Foundation School Of Engineering and Applied Science) t)
+	(test-one-match '(The * School Of Engineering and Applied Science) '(The Fu Foundation School Of Engineering and Applied Science) t)
+	(test-one-match '(The * School Of Engineering and Applied Science) '(The School Of Engineering and Applied Science) t)
+	(test-one-match '(* 3 ?x 4 *) '(3 5 4) '((?x 5)))
+	(test-one-match '(?x (1 2) ?y (4 5)) '(c (1 2) d (4 5)) '((?y d)(?x c)))
+	(test-one-match '(?y ?z (c v)) '(8 gh (c v) ) '((?z gh)(?y 8)))
+	(test-one-match '(((get) me) out) '(get (me (out))) NIL)
+	(test-one-match '(A * B) '(A A A A A B) t)
+	(test-one-match '(?x * ?y) '(A A A A A B) '((?y b)(?x a)))
+	(test-one-match '(? ? ?) '(a (a b) ((a b c))) t)
+	(test-one-match '(a * ?x *) '(a b c d) '(((?x b)) ((?x c)) ((?x d))))
+	"DONE!")
+
+;; individual test, prints results
+(defun test-one-match (pattern data output)
+	(print "---------- RUNNING TEST -----------")
+	(print (format NIL "pattern: ~a" pattern))
+	(print (format NIL "data:    ~a" data))
+	(let ((result (match pattern data)))
+		(if (equal result output)
+			(print "TEST PASSED")
+			(progn
+				(print "**********************************")
+				(print "************ FAILED **************")
+				(print "**********************************")
+				(print (format NIL "expected: ~a" output))
+				(print (format NIL "result:   ~a" result))))))
+
+;; pattern matching algorithm
 (defun match (pattern data)
 
 	; base case: pattern is empty
@@ -9,11 +49,16 @@
 			(return-from match t)
 			(return-from match NIL)))
 
-	; base case: data is empty
 	(when (= 0 (length data))
-		(if (equal (first pattern) '*)
-			(return-from match (match (rest pattern) data))
+		(if (every #'(lambda (p) (equal p '*)) pattern)
+			(return-from match t)
 			(return-from match NIL)))
+
+	; base case: data is empty
+;	(when (= 0 (length data))
+;		(if (equal (first pattern) '*)
+;			(return-from match (match (rest pattern) data))
+;			(return-from match NIL)))
 
 	(let ((p (first pattern)) (d (first data)))
 		
@@ -61,46 +106,3 @@
 
 			; else no match
 			(NIL))))
-
-(defun test-match ()
-	(test-one-match '() '() t)
-	(test-one-match '(ai) '(ai) t)
-	(test-one-match '(ai cs) '(ai cs) t)
-	(test-one-match '(cs ai) '(ai cs) NIL)
-	(test-one-match '(1 2 3 0) '(1 2 3 4 0) NIL)
-	(test-one-match '(? mudd) '(seely mudd) t)
-	(test-one-match '(?first ?middle mudd) '(seely w mudd) '((?middle w) (?first seely)))
-	(test-one-match '(? ?x ? ?y ?) '(Warren Buffet Is A Good Man) NIL)
-	(test-one-match '(School Of Engineering and Applied Science) '(School Of Engineering) NIL)
-	(test-one-match '(* School Of Engineering and Applied Science) '(The Fu Foundation School Of Engineering and Applied Science) t)
-	(test-one-match '(The * School Of Engineering and Applied Science) '(The Fu Foundation School Of Engineering and Applied Science) t)
-	(test-one-match '(The * School Of Engineering and Applied Science) '(The School Of Engineering and Applied Science) t)
-	(test-one-match '(* 3 ?x 4 *) '(3 5 4) '((?x 5)))
-	(test-one-match '(?x (1 2) ?y (4 5)) '(c (1 2) d (4 5)) '((?y d)(?x c)))
-	(test-one-match '(?y ?z (c v)) '(8 gh (c v) ) '((?z gh)(?y 8)))
-	(test-one-match '(((get) me) out) '(get (me (out))) NIL)
-	(test-one-match '(A * B) '(A A A A A B) t)
-	(test-one-match '(?x * ?y) '(A A A A A B) '((?y b)(?x a)))
-	(test-one-match '(? ? ?) '(a (a b) ((a b c))) t)
-	(test-one-match '(a * ?x *) '(a b c d) '(((?x b)) ((?x c)) ((?x d))))
-	"DONE!")
-
-(defun test-one-match (pattern data output)
-	
-	; log the inputs
-	(print (format NIL "pattern: ~a" pattern))
-	(print (format NIL "data:    ~a" data))
-
-	; check the output
-	(let ((result (match pattern data)))
-		(if (equal result output)
-			(print "TEST PASSED")
-			(progn
-				(print "******** TEST FAILED ********")
-				(print (format NIL "expected: ~a" output))
-				(print (format NIL "result:   ~a" result)))))
-
-	; skip a line
-	(print ""))
-
-
